18f4940ce6c9a31d3de0c983c000251f0e765ecb
mikeygbd <mikey.soares@gmail.com>
2019-10-14 19:29:26 -0400

    inported FlatList and changed useContext to blogPosts and renedered a flatlist item with the key of the blog post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,15 +3,22 @@ import {
   View,
   Text,
   StyleSheet,
+  FlatList
 } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = ({}) => {
-  const value = useContext(BlogContext)
+  const blogPosts = useContext(BlogContext)
   return (
   <View style={styles.container}>
     <Text>I'm Index Screen</Text>
-    <Text>The value is :{value}</Text>
+    <FlatList
+    data={blogPosts}
+    keyExtractor={(blogPost) => blogPost.title}
+    renderItem={({ item }) => {
+      return <Text>{item.title}</Text>
+    }}
+    />
 
   </View>
 )
