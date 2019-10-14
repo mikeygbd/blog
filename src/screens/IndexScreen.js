0ef9edf5a6dc9c51ff9db267818fc6ad29acc304
mikeygbd <mikey.soares@gmail.com>
2019-10-14 19:47:26 -0400

    imported Button. changed blogPosts to data and addBlogPost created a button and changed data in flatList

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,17 +3,19 @@ import {
   View,
   Text,
   StyleSheet,
-  FlatList
+  FlatList,
+  Button
 } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = ({}) => {
-  const blogPosts = useContext(BlogContext)
+  const { data, addBlogPost } = useContext(BlogContext)
   return (
   <View style={styles.container}>
     <Text>I'm Index Screen</Text>
+    <Button title="Add Post" onPress={addBlogPost}/> 
     <FlatList
-    data={blogPosts}
+    data={data}
     keyExtractor={(blogPost) => blogPost.title}
     renderItem={({ item }) => {
       return <Text>{item.title}</Text>
