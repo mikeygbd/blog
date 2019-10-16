fa0401ceec1aec589eaaca37b0dbb3aed7308b2d
mikeygbd <mikey.soares@gmail.com>
2019-10-16 07:54:19 -0400

    added a trashcan Icon and added some styling with top border and increased Icon and title size

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -7,18 +7,21 @@ import {
   Button
 } from 'react-native';
 import { Context } from '../context/BlogContext';
+import {Entypo} from  '@expo/vector-icons'
 
 const IndexScreen = ({}) => {
   const {state, addBlogPost } = useContext(Context)
   return (
   <View style={styles.container}>
-    <Text>I'm Index Screen</Text>
     <Button title="Add Post" onPress={addBlogPost}/>
     <FlatList
     data={state}
     keyExtractor={(blogPost) => blogPost.title}
     renderItem={({ item }) => {
-      return <Text>{item.title}</Text>
+      return <View style={styles.row}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Entypo style={styles.icon} name="trash" />
+      </View>
     }}
     />
 
@@ -30,5 +33,20 @@ export default IndexScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  row: {
+    borderTopWidth: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderColor: 'gray',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontSize: 18,
+
+  },
+  icon: {
+    fontSize: 24,
   }
 });
