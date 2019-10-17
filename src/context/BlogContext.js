3090848954d74e03efa9ec55206d9ba11ab83dbb
mikeygbd <mikey.soares@gmail.com>
2019-10-17 09:15:08 -0400

    added title and content to addBlogPost function and dispatched them as the payload. then added them to the case statement as the action payload

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -9,7 +9,9 @@ const blogReducer = (state, action) => {
       ...state,
       {
       id: (Math.random() * 99999),
-       title: `Blog Post #${state.length + 1}`
+       title: action.payload.title,
+       content: action.payload.content
+
      }
    ]
     default:
@@ -18,8 +20,8 @@ const blogReducer = (state, action) => {
 }
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blogpost'})
+  return (title, content) => {
+    dispatch({ type: 'add_blogpost', payload: {title, content}})
   }
 }
 
