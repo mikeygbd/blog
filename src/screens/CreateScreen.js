399a0be66f73d90491f668d2df6dc04ee6b5b2ad
mikeygbd <mikey.soares@gmail.com>
2019-10-17 09:16:22 -0400

    imported text input and button and added an onPress of addBlogPost with the arguments of title amd content

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,16 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {
   View,
   Text,
   StyleSheet,
+  TextInput,
+  Button
 } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = ({ navigation }) => {
-
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const {addBlogPost} = useContext(Context)
 return (
   <View style={styles.container}>
-    <Text>Create</Text>
+    <Text style={styles.label}>Enter Title:</Text>
+    <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
+    <Text style={styles.label}>Enter Content:</Text>
+    <TextInput style={styles.input} value={content} onChangeText={text => setContent(text)}/>
+    <Button
+      title="Add Blog Post"
+      onPress={() => addBlogPost(title, content)}
+    />
   </View>
 );
 }
@@ -19,5 +30,18 @@ export default CreateScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 10,
   },
+  input: {
+    padding: 10,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+
 });
