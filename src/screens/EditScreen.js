7b18aedf22da05683073f57a3546d2b334306212
mikeygbd <mikey.soares@gmail.com>
2019-10-18 08:47:00 -0400

    imported usState and useContext and destructured navigation set state to useContext and blogPost to find the getParam I passed in. set title with initial state of blogpost.title and same for content

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import {
   View,
   Text,
   StyleSheet,
+  TextInput
 } from 'react-native';
+import {Context} from '../context/BlogContext'
 
-const EditScreen = ({}) => (
+const EditScreen = ({ navigation }) => {
+
+  const { state } = useContext(Context)
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+
+  const [title, setTitle] = useState(blogPost.title)
+
+  const [content, setContent] = useState(blogPost.content)
+
+  return (
   <View style={styles.container}>
-    <Text>I'm EditScreen</Text>
+    <Text>Edit Title:</Text>
+    <TextInput value={title} onChange={(newTitle) => setTitle(newTitle)} />
+
   </View>
 );
-
+}
 export default EditScreen;
 
 const styles = StyleSheet.create({
