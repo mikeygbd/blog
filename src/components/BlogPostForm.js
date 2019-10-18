785adaa75b53faa6fb10d7a0b96d002025a0dab8
mikeygbd <mikey.soares@gmail.com>
2019-10-18 09:11:28 -0400

    copy and pasted jsx for form to create post destructured onSubmit and called it in the onPress

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button
+} from 'react-native';
+
+const BlogPostForm = ({ onSubmit }) => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput style={styles.input} value={content} onChangeText={text => setContent(text)}/>
+      <Button
+        title="Save Blog Post"
+        onPress={() => onSubmit(title, content)}
+      />
+    </View>
+  );
+}
+
+export default BlogPostForm;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    padding: 10,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+});
