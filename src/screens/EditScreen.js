d17dba4b83e2495ec8153d7c6474d052ce4a1cf9
mikeygbd <mikey.soares@gmail.com>
2019-10-18 09:07:50 -0400

    imported and returned BlogPostForm

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,11 +1,9 @@
-import React, {useState, useContext} from 'react';
+import React, { useContext } from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
-  TextInput
 } from 'react-native';
 import {Context} from '../context/BlogContext'
+import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
 
@@ -13,17 +11,7 @@ const EditScreen = ({ navigation }) => {
 
   const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
 
-  const [title, setTitle] = useState(blogPost.title)
-
-  const [content, setContent] = useState(blogPost.content)
-
-  return (
-  <View style={styles.container}>
-    <Text>Edit Title:</Text>
-    <TextInput value={title} onChange={(newTitle) => setTitle(newTitle)} />
-
-  </View>
-);
+  return <BlogPostForm />
 }
 export default EditScreen;
 
