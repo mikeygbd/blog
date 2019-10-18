c5908dbb616df616f3b52059c930b9255d182f19
mikeygbd <mikey.soares@gmail.com>
2019-10-18 09:10:01 -0400

    imported and returned BlogPostForm and passed in a prop if onSubmit with the arguments of title and content then called addBlogpost with a third argument of a callback to navigate to Index after

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,34 +1,18 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  TextInput,
-  Button
 } from 'react-native';
 import { Context } from '../context/BlogContext';
-
+import BlogPostForm from '../components/BlogPostForm';
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
-  const {addBlogPost} = useContext(Context)
-return (
-  <View style={styles.container}>
-    <Text style={styles.label}>Enter Title:</Text>
-    <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
-    <Text style={styles.label}>Enter Content:</Text>
-    <TextInput style={styles.input} value={content} onChangeText={text => setContent(text)}/>
-    <Button
-      title="Add Blog Post"
-      onPress={() => {
-        addBlogPost(title, content, () => {
-          navigation.navigate('Index')
-        })
 
-      }}
-    />
-  </View>
-);
+  const {addBlogPost} = useContext(Context)
+
+  return <BlogPostForm onSubmit={(title, content) => {
+    addBlogPost(title, content, () => navigation.navigate('Index'))
+  }}
+  />
 }
 export default CreateScreen;
 
@@ -37,16 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 10,
   },
-  input: {
-    padding: 10,
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 15
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-  },
+
 
 });
