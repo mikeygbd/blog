7545064f77fde796bdfc86a7725aa4ca03fdd322
mikeygbd <mikey.soares@gmail.com>
2019-10-19 07:57:14 -0400

    imported jsonServer created a function getBlogPosts and created case statement just returning the action.payload

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext'
+import jsonServer from '../api/jsonServer'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts':
+    return action.payload
     case 'edit_blogpost':
     return state.map((blogPost) => {
       return blogPost.id === action.payload.id ? action.payload : blogPost
@@ -20,6 +23,13 @@ const blogReducer = (state, action) => {
    ]
     default:
     return state
+  }
+}
+
+const getBlogPosts = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('./blogPosts')
+    dispatch({ type: 'get_blogposts', payload: response.data})
   }
 }
 
@@ -47,4 +57,4 @@ const editBlogPost = (dispatch) => {
   }
 }
 
-export const { Context, Provider } = createDataContext(blogReducer, {addBlogPost, deleteBlogPost, editBlogPost}, [{ title: 'Test Post', content: 'Test Content', id: 1}])
+export const { Context, Provider } = createDataContext(blogReducer, {addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts}, [])
