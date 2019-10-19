4505e48401aae99fe2cbe986ca992551b263463f
mikeygbd <mikey.soares@gmail.com>
2019-10-19 08:39:30 -0400

    changed useEffect by adding a listener for when we go back to index screen with didFocus and then removed that listener when index screen is no longer running in the background

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -15,7 +15,14 @@ const IndexScreen = ({ navigation }) => {
   const {state, deleteBlogPost, getBlogPosts } = useContext(Context)
 
   useEffect(() => {
+    getBlogPosts()
+
+    const listener = navigation.addListener('didFocus', () => {
       getBlogPosts()
+    })
+    return () => {
+      listener.remove()
+    }
   }, [])
 
 
