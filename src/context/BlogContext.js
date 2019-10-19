50f1546317c60c8b464582c971da922fd86a19bf
mikeygbd <mikey.soares@gmail.com>
2019-10-19 08:43:49 -0400

    deleted unwanted case for add_blogpost and changed deleteBlogPost with a deleterequest with the id and dispatched same type removing it from state

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,16 +11,6 @@ const blogReducer = (state, action) => {
     })
     case 'delete_blogpost':
     return state.filter((blogPost) => blogPost.id !== action.payload)
-    case 'add_blogpost':
-    return [
-      ...state,
-      {
-      id: (Math.random() * 99999),
-       title: action.payload.title,
-       content: action.payload.content
-
-     }
-   ]
     default:
     return state
   }
@@ -36,7 +26,6 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callback) => {
     await jsonServer.post('/blogposts', { title, content })
-    //dispatch({ type: 'add_blogpost', payload: {title, content}})
     if (callback) {
       callback()
     }
@@ -44,7 +33,8 @@ const addBlogPost = (dispatch) => {
 }
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`)
   dispatch({ type: 'delete_blogpost', payload: id })
   }
 }
